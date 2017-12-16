4e0b4e786caf7fb264318f3be61fbee8b7daaeaf
nagielhachem <nagielhachem@gmail.com>
2017-12-16 15:41:05 +0100

    added random solved board initializer

--- src/tetravex.cpp
+++ src/tetravex.cpp
@@ -1,7 +1,8 @@
 #include "tetravex.hpp"
 
+#include <stdlib.h>
 
-Tetravex::Tetravex(int width, int height)
+Tetravex::Tetravex(int width, int height, int nb_values)
 {
 	if (width <= 0 || height < 0)
 	{
@@ -11,6 +12,7 @@ Tetravex::Tetravex(int width, int height)
 	if (height == 0)
 		height = width;
 	this->height = height;
+	this->nb_values = nb_values;
 	this->squares = std::vector<Square>(this->width * this->height);
 }
 
@@ -24,21 +26,66 @@ void Tetravex::set_squares(std::vector<Square> squares)
 	this->squares = squares;
 }
 
-std::ostream& operator<<(std::ostream& o, const Tetravex& tetravex);
+std::vector<Square> Tetravex::get_squares() const
 {
+	return this->squares;
+}
+
+int Tetravex::get_width() const
+{
+	return this->width;
+}
+
+int Tetravex::get_height() const
+{
+	return this->height;
+}
+
+
+void Tetravex::random_solved_init()
+{
+	// build solved tetravex then permute members
+	// random on East and South
+	// random on North only in first row
+	// random on West only in first column
+	// copy North and West from neighbour elsewhere
+	srand(time(NULL));
 	for (int i = 0; i < this->height; ++i)
+	{
+		for (int j = 0; j < this->width; ++j)
+		{
+			int k = i * this->width + j;
+			this->squares[k].values[E] = rand() % this->nb_values;
+			this->squares[k].values[S] = rand() % this->nb_values;
+			
+			if (i == 0) 
+				this->squares[k].values[N] = rand() % this->nb_values;
+			else 
+				this->squares[k].values[N] = this->squares[(i - 1) * this->width + j].values[S];
+			if (j == 0)
+				this->squares[k].values[W] = rand() % this->nb_values;
+			else 
+				this->squares[k].values[W] = this->squares[i * this->width + (j - 1)].values[E];
+		}
+	}
+}
+
+std::ostream& operator<<(std::ostream& o, const Tetravex& tetravex)
+{
+	for (int i = 0; i < tetravex.get_height(); ++i)
 	{
 		for (int j = 0; j < 3; ++j)
 		{
-			for (int k = 0; k < this->width; ++k)
+			for (int k = 0; k < tetravex.get_width(); ++k)
 			{
-				Square sq = this->squares[i * this->width + k];
+				Square sq = tetravex.get_squares()[i * tetravex.get_width() + k];
 				if (j == 1)
 					o << sq.values[3] << " " << sq.values[1];
 				else
 					o << " " << sq.values[j] << " ";
 				o << " ";
 			}
+			o << "\n";
 		}
 		o << "\n";
 	}
--- src/tetravex.hpp
+++ src/tetravex.hpp
@@ -1,23 +1,34 @@
 #include <vector>
 #include <iostream>
 
+#define N 0
+#define E 1
+#define S 2
+#define W 3
+
 struct Square
 {
 	// Edge values order: N-E-S-W
 	float values [4];
-}
+};
 
 class Tetravex
 {
 	public:
-		Tetravex(int width, int height = 0);
+		Tetravex(int width, int height = 0, int nb_values = 10);
 
 		void set_squares(std::vector<Square> squares);
-
+		std::vector<Square> get_squares() const;
+		int get_width() const;
+		int get_height() const;
+		
+		void random_solved_init();
+	
 	private:
 		int width;
 		int height;
+		int nb_values;
 		std::vector<Square> squares;
-}
+};
 
 std::ostream& operator<<(std::ostream& o, const Tetravex& tetravex);
