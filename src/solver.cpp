3f7062b82801463056a40346d0d3e6ec12c33ff9
nagielhachem <nagielhachem@gmail.com>
2017-12-17 10:21:08 +0100

    added solve method

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,35 +1,91 @@
 #include "solver.hpp"
 
-#include <iostream>
-#include <math.h>
+#include <algorithm>
 
-double proba_transition(double delta_U, double T)
+#include <math.h>
+#include <stdlib.h>
+
+Solver::Solver()
+{
+	std::random_device rd;  
+	this->gen = std::mt19937(rd()); 
+	this->dist = std::uniform_real_distribution<double>(0.0, 1.0);
+}
+
+double Solver::transition_prob(double delta_U, double T)
 {
 	return exp(delta_U / T);
 }
 
-double energie(Tetravex& t)
+int Solver::sample(double delta_U, double T)
+{
+	double p = this->transition_prob(delta_U, T);
+	if (this->dist(this->gen) < p)
+		return 1;
+	return 0;
+}
+
+double Solver::get_U(const std::vector<Piece>& pieces, const int width, const int height)
 {
 	double U = 0;
-	std::vector<Piece> pieces = t.get_pieces();
-	for (int i = 0; i < t.get_height(); ++i)
+	for (int i = 0; i < height; ++i)
 	{
-		for (int j = 0; j < t.get_width(); ++j)
+		for (int j = 0; j < width; ++j)
 		{
-			int k = i * t.get_width() + j;
-
+			int k = i * width + j;
 			if (i != 0) 
-				if (pieces[k].values[N] != pieces[(i - 1) * t.get_width() + j].values[S])
+				if (pieces[k].values[N] != pieces[(i - 1) * width + j].values[S])
 					U += 1;
 			if (j != 0)
-				if (pieces[k].values[W] != pieces[i * t.get_width() + (j - 1)].values[E])
+				if (pieces[k].values[W] != pieces[i * width + (j - 1)].values[E])
 					U += 1;
 		}
 	}
 	return U;
 }
 
-void solve(Tetravex& t)
+double Solver::init_T(Tetravex& t)
 {
+	// TODO
+	return 100;
+}
+
+void Solver::solve(Tetravex& t, double lambda, int verbose)
+{
+	t.random_shuffle();
+	double T = this->init_T(t);
+
+	std::vector<Piece> pieces = t.get_pieces();
+	int width = t.get_width();
+	int height = t.get_height();
+	int n = height * width;
+
+	double U1 = get_U(pieces, width, height);
+	while (U1 != 0)
+	{
+		// 1- Choose two different random axes
+		int i1 = rand() % n;
+		int i2 = i1;
+		while (i2 == i1)
+			i2 = rand() % n;
+	
+		// 2- swap axis
+		std::iter_swap(pieces.begin() + i1, pieces.begin() + i2);
+		
+		// 3- delta_u
+		double U2 = get_U(pieces, width, height);
+		double delta_U = U2 - U1;
+
+		if (delta_U < 0) // 4- choose candidate
+			t.set_pieces(pieces);
+		else if (sample(delta_U, T)) // choose candidate based on transition proba
+			t.set_pieces(pieces);
+
+		if (verbose)
+			std::cout << t;
+
+		T *= lambda;
+		U1 = get_U(pieces, width, height);
+	}
 
 }
--- src/solver.hpp
+++ src/solver.hpp
@@ -2,4 +2,25 @@
 
 #include "tetravex.hpp"
 
-void solve(Tetravex& t);
+#include <iostream>
+#include <vector>
+#include <random>
+
+class Solver{
+
+	public:
+		Solver();
+
+		void solve(Tetravex& t, double lambda = 0.99, int verbose = 0);
+
+		double get_U(const std::vector<Piece>& pieces, const int width, const int height);
+		double init_T(Tetravex& t);
+
+	private:
+		std::mt19937 gen;
+		std::uniform_real_distribution<double> dist;
+
+	  double transition_prob(double delta_U, double T);
+		int sample(double delta_U, double T);
+};
+
