30ad157b47fcd442e44c124e90e6117dd67eddfb
nagielhachem <nagielhachem@gmail.com>
2017-12-20 14:50:33 +0100

    better help/usage message

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,28 @@
 
 void usage(char* file)
 {
-	std::cout << "Usage: " << file << std::endl;
-	std::cout << "options: " << std::endl;
-	std::cout << "-w width: set width" << std::endl;
-	std::cout << "-h height: set height" << std::endl;
-	std::cout << "-l lambda: set lambda" << std::endl;
-	std::cout << "-m nb_interations: set max number of iterations" << std::endl;
-	std::cout << "-v : set verbose" << std::endl;
+	std::cout << "Usage:\n" 																							 
+					  << "\t" << file << " [-w width -h height -l lambda -m max_iter -v]\n"
+						<< "Help:\n"
+						<< "\t" << file << " --help\n";
+}
 
+void help(char* file)
+{
+	std::cout << "Usage:\n" 																							 
+					  << "\t" << file << " [-w width -h height -l lambda -m max_iter -v]\n"
+						<< "\nDesctiption:\n" 																				 
+						<< "\t-w width    : set board width to width\n" 												 
+						<< "\t-h height   : set board height to height\n" 											 
+						<< "\t-l lambda   : set lambda to lambda\n" 											 
+						<< "\t-m max_iter : set max number of iterations to max_iter (-1 <=> inf)\n" 		 
+						<< "\t-v          : set verbose to true\n"
+						<< "\nDefault Values:\n"
+						<< "\twidth    : 3\n" 												 
+						<< "\theight   : 3\n" 											 
+						<< "\tlambda   : 0.99\n" 											 
+						<< "\tmax_iter : inf\n" 		 
+						<< "\tverbose	 : false\n";
 }
 
 int parse(int argc, char** argv, int& w, int& h, int& v, float& l, int& m)
@@ -28,20 +42,34 @@ int parse(int argc, char** argv, int& w, int& h, int& v, float& l, int& m)
 	for (int i = 1; i < argc; ++i)
 	{
 		if (strcmp(argv[i], "-w") == 0)
-			w = atoi(argv[i + 1]);
-		if (strcmp(argv[i], "-h") == 0)
-			h = atoi(argv[i + 1]);
-		if (strcmp(argv[i], "-v") == 0)
+			w = atoi(argv[++i]);
+		else if (strcmp(argv[i], "-h") == 0)
+			h = atoi(argv[++i]);
+		else if (strcmp(argv[i], "-v") == 0)
 			v = 1;
-		if (strcmp(argv[i], "-l") == 0)
-			l = atof(argv[i + 1]);
-		if (strcmp(argv[i], "-m") == 0)
-			m = atoi(argv[i + 1]);
-		if (strcmp(argv[i], "--help") == 0)
+		else if (strcmp(argv[i], "-l") == 0)
+			l = atof(argv[++i]);
+		else if (strcmp(argv[i], "-m") == 0)
+			m = atoi(argv[++i]);
+		else if (strcmp(argv[i], "--help") == 0)
+		{
+			help(argv[0]);
+			return 0;
+		} 
+		else
 		{
 			usage(argv[0]);
 			return 0;
 		}
+	}
+	if (w <= 0 || h <= 0 || l <= 0.0 || l >= 1.0 || m < -1) // error in atoi or atof
+	{
+		std::cout << "Error:\n"
+							<< "\twidth, height must be a positive integers\n"
+							<< "\tmax_iter must be a positive integer or -1\n"
+							<< "\tlambda must be a float contained in ]0, 1[\n";
+		usage(argv[0]);
+		return 0;
 	}
 	return 1;
 }
